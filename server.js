517ca1ef76c8f8d124eed0ff0fdd49d89632b7f0
System <none>
2017-07-11 20:41:09 +0000

    🚦🏤 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,25 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.send( {'ip-address': req.headers['x-forwarded-for'].split(',')[0],
+             'software': req.headers['user-agent'],
+             'language': req.headers['accept-language'].substr(0,5)
+            })
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
